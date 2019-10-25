2dd2a27d5581597503aad170a15d0cc65de35049
lemonCjlu <birth109@126.com>
2019-10-25 14:54:43 +0800

    Signed-off-by: lemonCjlu <birth109@126.com>

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     now = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
     day  = time.strftime('%Y-%m-%d',time.localtime(time.time()))
     result_path = "F:\\Report\\"
-    report_file = result_path + "\\"+ now + "_result.html"
+    report_file =  "index.html"
     fp  = open(report_file,'wb')
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u"appium测试报告",description=u'用例详情;')
     runner.run(suite)
